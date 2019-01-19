8e5af4d874887e34daa79b72d8fa60d71a320803
katetrofimova <ktrofimova@protonmail.com>
2019-01-20 01:37:22 +0300

    path fix

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-CONT_LOC=/Users/katetrofimova/Downloads/Docker_Tutorial-master/basic_tutorial/
-docker run -it --name container --net=host -v $CONT_LOC:/ds hadron-collider-ml-week2
+docker run -it --name container --net=host -v $PWD:/ds hadron-collider-ml-week2
